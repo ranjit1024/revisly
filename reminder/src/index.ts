a1b1e1bd7c67f398e1fdb50fcdf6c317d81f3a04
ranjit <ranjitdas2048@gmail.com>
2025-09-06 22:58:07 +0530

    tommorow i have to move to hono to check

--- reminder/src/index.ts
+++ reminder/src/index.ts
@@ -1,5 +1,20 @@
-import {Redis} from "@upstash/redis"
+import { Redis } from "@upstash/redis"
 import dotenv from "dotenv"
+import { PrismaClient } from "@prisma/client";
 dotenv.config();
-console.log(process.env.S3_BUCKET)
-Redis.fromEnv()
+const redis = Redis.fromEnv();
+const prisma = new PrismaClient()
+async function checkDate() {
+
+    const date = await prisma.revisionSession.findMany({
+        select: {
+            topic:true,
+            email:true,
+            reminderDate: true
+        }
+    })
+     date.forEach(data => {
+        console.log(data.reminderDate.getTime(), data.topic)
+     })
+}
+checkDate()
