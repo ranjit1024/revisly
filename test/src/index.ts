102066221bb02c6474d0cc273560e3ba24a29ef1
ranjit <ranjitdas2048@gmail.com>
2025-11-23 22:45:31 +0530

    done with

--- test/src/index.ts
+++ test/src/index.ts
@@ -3,6 +3,7 @@ import { createClient } from "redis";
 import fs from "fs";
 import { Groq } from "groq-sdk";
 dotenv.config();
+
 class RevionsTest {
   private intervel = 3000;
   private revisionData: {
@@ -34,32 +35,29 @@ class RevionsTest {
       console.log(this.count);
       // getting data from redis queue and store in private veriable
       const reminderData = await redis.brPop("reminder", 60);
-      const revisionInfo: {
-        topic: string;
-        email: string;
-        revision_id: string;
-        id: string;
-      } = JSON.parse(reminderData?.element || "");
-      this.revisionData = revisionInfo;
+      this.revisionData = JSON.parse(reminderData?.element || "");
       console.log(this.revisionData);
-      const notes = await this.generateNotes(this.revisionData.topic);
-      // console.log(notes)
-      // done here
+      if (this.revisionData) {
+        const notes = await this.generateNotes(this.revisionData.topic);
+        this.Store('data.json', this.revisionData);
+        this.Store('questions.json', notes);
+        
+      }
     }, this.intervel);
   };
 
   // genertaing questions
   private generateNotes = async (topic: string) => {
-      const groq = new Groq({
-        apiKey: process.env.GROQ_API_KEY,
-      });
-      // genrta notes from LLM
-      const chatCompletion = await groq.chat.completions.create({
-        model: "openai/gpt-oss-20b",
-        messages: [
-          {
-            role: "user",
-            content: `Generate 10 mid to hard multiple-choice interview questions focusing on ${topic}Strictly output the result as a raw JSON array of objects. Do not include markdown formatting likeUse this exact JSON structure for each object:
+    const groq = new Groq({
+      apiKey: process.env.GROQ_API_KEY,
+    });
+    // generatea Questions from LLM
+    const chatCompletion = await groq.chat.completions.create({
+      model: "openai/gpt-oss-20b",
+      messages: [
+        {
+          role: "user",
+          content: `Generate 10 mid to hard multiple-choice interview questions focusing on ${topic}Strictly output the result as a raw JSON array of objects. Do not include markdown formatting likeUse this exact JSON structure for each object:
             {
               "id": <number>,
               "question": "<The technical question string>",
@@ -71,20 +69,19 @@ class RevionsTest {
               ],
               "correctAnswer": <index of the correct option, e.g., 0 for A, 1 for B>
             }`,
-          },
-        ],
-      });
-      // storing in loca 
-      const question = fs.writeFile("questions.json",chatCompletion.choices[0].message.content || "{}", 'utf-8', (err)=>{
-        if(err){
-          console.log("Error while Storing",err)
-        }
-        else{
-          console.log("Data successfully stored")
-        }
-      } )
-      return chatCompletion.choices[0].message.content;
-    
+        },
+      ],
+    });
+    return chatCompletion.choices[0].message.content;
+  };
+  // storing in hard disk
+  private Store = (filename: string, content:any) => {
+    try {
+      fs.writeFileSync(filename, content, "utf-8");
+      console.log("file Written Successfully");
+    } catch (err) {
+      console.log("Error while storing file", err);
+    }
   };
   // stoping the intervel
   private stop = () => {
