055467b9da822a9eb8323b2eb3f98c1e81109a20
ranjit <ranjitdas2048@gmail.com>
2025-11-28 22:30:44 +0530

    it should work

--- revision/app/revisly/revision/page.tsx
+++ revision/app/revisly/revision/page.tsx
@@ -132,7 +132,7 @@ function Mobile() {
                 console.log(sessionData);
                 const setRevision = await axios.post(
 
-                  "http://localhost:3000/api/revision",
+                  "https://www.revisly.in/api/revision",
                   sessionData,
                   {
                     headers: {
@@ -309,7 +309,7 @@ function Desktop() {
                   setSendData(true);
                   console.log(sessionData);
                   const setRevision = await axios.post(
-                    "http://localhost:3000/api/revision",
+                    "https://www.revisly.in/api/revision",
                     sessionData,
                     {
                       headers: {
