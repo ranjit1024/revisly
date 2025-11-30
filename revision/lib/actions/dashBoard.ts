3110d217bc7c1507836cbf6164da2600e2f203bd
ranjit <ranjitdas2048@gmail.com>
2025-11-30 09:48:20 +0530

    fixed currnet topoic card

--- revision/lib/actions/dashBoard.ts
+++ revision/lib/actions/dashBoard.ts
@@ -3,6 +3,7 @@ import prisma from "../prisma"
 import { getServerSession } from "next-auth"
 import { authOption } from "../auth"
 import { da } from "date-fns/locale";
+import { Nimble } from "aws-sdk";
 export async function getuserData(){
     const session = await getServerSession(authOption);
     // get total session count
@@ -44,18 +45,51 @@ export async function currentTopic(){
             brif:true
         }
     })
-    const progressData = await prisma.revisionSession.findMany({
-        where:{
-            topic:Topic?.topic
-        },
-        select:{
-            status:true
-        }
-    })
+    if(Topic === null){
+        const completedTopic = await prisma.revision.findFirst({
+             where:{
+                email:session?.user?.email || "",
+                status:"PENDING" 
+            },
+            
+            select:{
+                topic:true,
+                brif:true
+            }
+        })
+        const progressData = await prisma.revisionSession.findMany({
+            where:{
+                topic:completedTopic?.topic
+            },
+            select:{
+                status:true
+            }
+        })
+    
+   
+    const progress = progressData.filter(data =>  data.status === "COMPLETED").length 
+
+    const result = {...currentTopic, progress  }
+
+    console.log(result);
+    return result;
+    }
+    else{
+         const progressData = await prisma.revisionSession.findMany({
+            where:{
+                topic:Topic?.topic
+            },
+            select:{
+                status:true
+            }
+        })
+    
+   
     const progress = progressData.filter(data =>  data.status === "COMPLETED").length 
 
     const result = {...Topic, progress  }
 
     console.log(result);
     return result;
+    }
 }
