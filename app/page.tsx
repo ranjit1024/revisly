c066ef405f39127e227d028408930a107f77743b
ranjit1024 <ranjitdas2048@gmail.com>
2026-04-11 22:07:49 +0530

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import Navbar from "./components/ui/header";
-import TrustBadge from "./components/ui/badge";
+import Navbar from "../components/ui/header";
+import TrustBadge from "../components/ui/badge";
 import { motion } from "framer-motion";
 import { ArrowRight } from "lucide-react";
 
